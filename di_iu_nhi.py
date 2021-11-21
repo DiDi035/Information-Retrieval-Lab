8906991c4651b041aca4c959ecc45747938b57b1
Huynh Bao Di <dihuynh@Huynhs-MacBook-Pro.local>
2021-11-21 22:08:25 +0700

    Anh yeu em <3

---
+++ di_iu_nhi.py
@@ -0,0 +1,51 @@
+import socket
+import threading
+
+host = socket.gethostbyname("localhost")
+port = 9989
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host, port))
+
+def write():
+    while True:
+        message = input("")
+        client.send(message.encode("ascii"))
+
+def receive():
+    while True:
+        message = client.recv(1024).decode("ascii")
+        message_arr = message.split("|")
+        prefix = message_arr[0]
+        if prefix == "question":
+            for item in message_arr:
+                if item != "question":
+                    print(item + "\n")
+        elif prefix == "remainquestion":
+            print(f"Remain questions: {message_arr[1]}\n")
+        elif prefix == "error":
+            print("This is not your turn !!!!!\n")
+        elif prefix == "turn":
+            print(f"{message_arr[1]}'s turn\n")
+        elif prefix == "correct":
+            print(f"{message_arr[1]}'s answer is correct !!!!!\n")
+        elif prefix == "incorrect":
+            print(f"{message_arr[1]}'s answer is wrong !!!!!\n")
+        elif prefix == "remainplayers":
+            for player in message_arr:
+                if player != "remainplayers":
+                    print(player + "\n")
+
+while True:
+    nickname = input("Choose a nickname: ")
+    client.send(nickname.encode("ascii"))
+    message = client.recv(1024).decode("ascii")
+    if message == "valid":
+        break
+    else:
+        print("This nickname is taken")
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import random
+import time
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -12,42 +13,67 @@ print(f"Listening the port {port}")
 server.bind((host, port))
 server.listen(100)
 
-NUMBER_OF_PLAYER = 3
+NUMBER_OF_PLAYER = 2
 clients = []
 nicknames = []
 question = 0
 turn = 0
 game_start = False
 
-Q = [" What is the Italian word for PIE? \n a.Mozarella b.Pasty c.Patty d.Pizza\n",
-     " Water boils at 212 Units at which scale? \n a.Fahrenheit b.Celsius c.Rankine d.Kelvin\n",
-     " Which sea creature has three hearts? \n a.Dolphin b.Octopus c.Walrus d.Seal\n",
-     " Who was the character famous in our childhood rhymes associated with a lamb? \n a.Mary b.Jack c.Johnny d.Mukesh\n",
-     " How many bones does an adult human have? \n a.206 b.208 c.201 d.196\n",
-     " How many wonders are there in the world? \n a.7 b.8 c.10 d.4\n",
-     " What element does not exist? \n a.Xf b.Re c.Si d.Pa\n",
-     " How many states are there in India? \n a.24 b.29 c.30 d.31\n",
-     " Who invented the telephone? \n a.A.G Bell b.John Wick c.Thomas Edison d.G Marconi\n",
-     " Who is Loki? \n a.God of Thunder b.God of Dwarves c.God of Mischief d.God of Gods\n",
-     " Who was the first Indian female astronaut ? \n a.Sunita Williams b.Kalpana Chawla c.None of them d.Both of them \n",
-     " What is the smallest continent? \n a.Asia b.Antarctic c.Africa d.Australia\n",
-     " The beaver is the national embelem of which country? \n a.Zimbabwe b.Iceland c.Argentina d.Canada\n",
-     " How many players are on the field in baseball? \n a.6 b.7 c.9 d.8\n",
-     " Hg stands for? \n a.Mercury b.Hulgerium c.Argenine d.Halfnium\n",
-     " Who gifted the Statue of Libery to the US? \n a.Brazil b.France c.Wales d.Germany\n",
-     " Which planet is closest to the sun? \n a.Mercury b.Pluto c.Earth d.Venus\n"]
+Q = [" What is the Italian word for PIE?|a.Mozarella|b.Pasty|c.Patty|d.Pizza",
+     " Water boils at 212 Units at which scale?|a.Fahrenheit|b.Celsius|c.Rankine|d.Kelvin",
+     " Which sea creature has three hearts?|a.Dolphin|b.Octopus|c.Walrus|d.Seal",
+     " Who was the character famous in our childhood rhymes associated with a lamb?|a.Mary|b.Jack|c.Johnny|d.Mukesh",
+     " How many bones does an adult human have?|a.206|b.208|c.201|d.196",
+     " How many wonders are there in the world?|a.7|b.8|c.10|d.4",
+     " What element does not exist?|a.Xf|b.Re|c.Si|d.Pa",
+     " How many states are there in India?|a.24|b.29|c.30|d.31",
+     " Who invented the telephone?|a.A.G Bell|b.John Wick|c.Thomas Edison|d.G Marconi",
+     " Who is Loki?|a.God of Thunder|b.God of Dwarves|c.God of Mischief|d.God of Gods",
+     " Who was the first Indian female astronaut ?|a.Sunita Williams|b.Kalpana Chawla|c.None of them|d.Both of them ",
+     " What is the smallest continent?|a.Asia|b.Antarctic|c.Africa|d.Australia",
+     " The beaver is the national embelem of which country?|a.Zimbabwe|b.Iceland|c.Argentina|d.Canada",
+     " How many players are on the field in baseball?|a.6|b.7|c.9|d.8",
+     " Hg stands for?|a.Mercury|b.Hulgerium|c.Argenine|d.Halfnium",
+     " Who gifted the Statue of Libery to the US?|a.Brazil|b.France|c.Wales|d.Germany",
+     " Which planet is closest to the sun?|a.Mercury|b.Pluto|c.Earth|d.Venus"]
 
 A = ['d', 'a', 'b', 'a', 'a', 'a', 'a', 'b', 'a', 'c', 'b', 'd', 'd', 'c', 'a', 'b', 'a']
 
 def broadcast(message):
     for client in clients:
         client.send(message.encode("ascii"))
+    time.sleep(0.2)
 
 def quiz():
     global question
 
     question = random.randint(0,10000)%len(Q)
-    broadcast(Q[question])
+    question_str = "question|"
+    question_str += Q[question]
+    broadcast(question_str)
+
+def remain_questions():
+    broadcast(f"remainquestion|{len(Q)}")
+
+def wrong_turn_error(turn):
+    clients[turn].send(f"error|{nicknames[turn]}".encode("ascii"))
+    time.sleep(0.2)
+
+def countdown():
+    broadcast("time|")
+
+def send_turn(turn):
+    broadcast(f"turn|{nicknames[turn]}")
+
+def correct(turn):
+    broadcast(f"correct|{nicknames[turn]}")
+
+def incorrect(turn):
+    broadcast(f"incorrect|{nicknames[turn]}")
+
+def remain_players():
+    broadcast("remainplayers|" + "|".join(nicknames))
 
 def client_thread(client):
     global turn
@@ -63,22 +89,25 @@ def client_thread(client):
             while current_client < len(clients):
                 if clients[current_client] == client:
                     break
-                current_client +=1
-            print(f"turn: {turn}")
-            print(f"current: {current_client}")
+                current_client += 1
             if current_client == turn:
                 if message == A[question]:
-                    broadcast(f"{nicknames[turn]}'s answer is correct !!!!\n")
+                    correct(turn)
+                    A.remove(A[question])
+                    Q.remove(Q[question])
                     quiz()
                 else:
-                    broadcast(f"{nicknames[turn]}'s answer is wrong !!!!\n")
+                    incorrect(turn)
 
                 turn+=1
                 if turn >= len(clients):
                     turn = 0
-                clients[turn].send("yourturn\n".encode("ascii"))
+
+                remain_players()
+                remain_questions()
+                send_turn(turn)
             else:
-                clients[current_client].send("You are only allowed to answer when it is your turn".encode("ascii"))
+                wrong_turn_error(current_client)
         else:
             remove(client)
 
@@ -90,8 +119,15 @@ while True:
     client, address = server.accept()
     print(f"Connected with {str(address)}")
 
-    client.send("NICKNAME".encode("ascii"))
-    nickname = client.recv(1024).decode("ascii")
+    while True:
+        nickname = client.recv(1024).decode("ascii")
+        if nickname:
+            if nickname not in nicknames:
+                client.send("valid".encode("ascii"))
+                break
+            else:
+                client.send("invalid".encode("ascii"))
+
     nicknames.append(nickname)
     clients.append(client)
 
@@ -100,5 +136,8 @@ while True:
 
     if len(clients) >= NUMBER_OF_PLAYER:
         game_start = True
-        clients[turn].send("yourturn\n".encode("ascii"))
+        broadcast(f"This is {nicknames[turn]}'s turn\n")
+        remain_players()
+        remain_questions()
+        send_turn(turn)
         quiz()
