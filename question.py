5339b08ba8538805dee14ba1511e95d337ac81d2
Khoi Ngo <ngominhkhoi2014@gmail.com>
2021-11-27 20:28:23 +0700

    fix answer of MQA to a,b,c,d

--- question.py
+++ question.py
@@ -2,16 +2,22 @@ def get_multiple_question_and_answer():
     path_file = './data/MQA.txt'
     question = []
     answer = []
-    
-    f = open(path_file, 'r') 
+
+    f = open(path_file, 'r')
     data_list = f.readlines()
     f.close()
-    
+
     for line in data_list:
         data_line = line.strip().split('#')
         question.append(data_line[0])
         answer.append(data_line[1])
-    
+
     return question, answer
 
-question,answer = get_multiple_question_and_answer()
+
+question, answer = get_multiple_question_and_answer()
+for i in range(len(question)):
+    print(question[i])
+    print(answer[i])
+# print(question)
+# print(answer)
